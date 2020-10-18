37ec3ff54655221df7504f845e767caa1f00d958
sharkbound <laptopblaster@gmail.com>
2020-10-18 18:44:40 -0500

    updated example main

--- examples/main.py
+++ examples/main.py
@@ -3,16 +3,11 @@ from dearpygui.simple import show_item
 from dpg_oop_wrapper import *
 from dearpygui.core import start_dearpygui
 
-tools = Tab('tools')
-other = Tab('other')
-b1 = Button('hai!')
-b2 = Button('bai!', callback=lambda *_: b2.hide())
-
 with TabBar('tabbar'):
-    with tools:
+    with Tab('tools') as tools:
         tools.tooltip = 'testing!'
-        b1.place()
-    with other:
-        b2.place()
+        b1 = Button('hai!').place()
+    with Tab('other'):
+        b2 = Button('bai!', callback=lambda *_: b2.hide()).place()
 
 start_dearpygui()
